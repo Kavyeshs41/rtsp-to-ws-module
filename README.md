aa3529b58dd4eb6da613508a734ecedc9ba4c7e3
systems-ap <systems@agriiprince.com>
2020-07-26 23:40:52 +0530

    feat: dynamic urls support for rtsp to ws streams

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # rtsp-to-ws-module
 This Module will be part of backend  which is helpful to use rtsp streaming over websockets
+
+Using base docker image from 
+https://github.com/kyriesent/node-rtsp-stream<br>
+https://hub.docker.com/r/kavyesh/node-rtsp-ws
+
+# To do
+-> Documentation<br>
+-> remove containers API<br>
+-> docker cleanup code<br>
+-> A lot work
+
+# Know Bugs
+
+-> docker handaling is not proper so still generating urls when daemon is off which leades in app crash<br>
---
+++ index.js
@@ -0,0 +1,46 @@
+var dotenv = require('dotenv').config();
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var app = express();
+var http = require('http');
+var server = http.createServer(app);
+
+
+
+// Json parser
+app.use(bodyParser.json({
+  limit: "2.7mb",
+  extended: false
+}));
+app.use(bodyParser.urlencoded({
+  limit: "2.7mb",
+  extended: false
+}));
+
+app.use(cors())
+
+app.all('/*', function (req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token, Authorization');
+  if (req.headers["accept-language"]) { // If header send language, then set to that language
+    i18n.setLocale(req.headers["accept-language"]);
+  }
+  if (req.method == 'OPTIONS') {
+    res
+      .status(200)
+      .end();
+  } else {
+    next();
+  }
+});
+
+app.use('/api/v1/sockets/', require('./routes/app'));
+
+app.set('port', process.env.PORT);
+server.listen(app.get('port'), function () {
+  console.log(" Application is running on " + process.env.PORT + " port....");
+});
